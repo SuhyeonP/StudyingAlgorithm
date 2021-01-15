22e9c9862ed927c2a6b6cedd8f1840791613c630
ahah12k <ahah12k@gmail.com>
2021-01-15 11:13:51 +0900

    studying vue

--- vue/webgame/webpack.config.js
+++ vue/webgame/webpack.config.js
@@ -12,7 +12,7 @@ module.exports={
     },
     module:{
         rules:[{
-            test:/\.vue$/,
+            test:/\.vue$/,//정규표현식을 이용해 파일확장자가 vue인것을 이용할수있음(js만 되는데 rules를 통해서 이용가능하게됨)
             use:'vue-loader',
         }],
     },
