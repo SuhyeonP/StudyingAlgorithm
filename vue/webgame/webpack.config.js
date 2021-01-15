0dc777bab9c7e3583a8b15ceb61dce1175a8b1eb
ahah12k <ahah12k@gmail.com>
2021-01-15 11:19:28 +0900

    studying vue

--- vue/webgame/webpack.config.js
+++ vue/webgame/webpack.config.js
@@ -13,11 +13,13 @@ module.exports={
     module:{
         rules:[{
             test:/\.vue$/,//정규표현식을 이용해 파일확장자가 vue인것을 이용할수있음(js만 되는데 rules를 통해서 이용가능하게됨)
-            use:'vue-loader',
+            use:'vue-loader',//vue파일을 만나면 vue-loader가 처리하게 되는것임
+            //loader:'vue-loader' 이렇게 use아니고 loader로 해도됨.
+            // vue랑 vue-template-compiler랑 버전 일치해야함
         }],
     },
     plugins:[
-        new VueLoaderPlugin(),
+        new VueLoaderPlugin(),//이거 안너으면 에러나는데 이유는 딱히 없....다고 한다......
     ],
     output:{
         filename:'[name].js',
